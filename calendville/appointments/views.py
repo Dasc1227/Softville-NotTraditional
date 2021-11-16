2a6a9cb71be046c5657fa0e0809a122ba100d518
luchor27 <luchoestebanra@gmail.com>
2021-11-15 19:30:21 -0600

    Add: Basic login and logout authentication, rearranged files

--- calendville/appointments/views.py
+++ calendville/appointments/views.py
@@ -1,12 +1,46 @@
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-# from appointments.models import Appointment
+from django.urls import reverse
 
 
 def index(request):
+    return render(request, "index.html")
 
-    return render(request, "index.html", {
-        "index": "Hola",
-        "title": "Title"
-    })
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication is successful
+        if user is not None:
+            login(request, user)
+            if request.POST.get("next"):
+                return HttpResponseRedirect(request.POST.get("next"))
+            else:
+                return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "login.html")
+
+
+@login_required(login_url='/login')
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register_appointment(request):
+    return render(request, "register_appointment.html")
+
+
