fdf0d1b7476d6059324bbd71f6fee26f2339df45
Steven Fernández <vsteven25@gmail.com>
2021-11-16 00:26:07 -0600

    Agregar interfaz de citas
    - Se agregaron metadatos al head y cambio de div por main
      en el layout para que el diseño se adapte a pantallas pequeñas
    - Se agregó la ruta y la función que consulta la base de
      datos para las citas de la próxima semana
    - Se asocian los usuarios del modelo Worker, puede ser
      necesario un cambio para asociar los datos usados para
      iniciar sesión

--- calendville/appointments/urls.py
+++ calendville/appointments/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('login', views.login_view, name="login"),
     path('logout', views.logout_view, name="logout"),
     path('register_appointment', views.register_appointment,
-         name="register_appointment")
+         name="register_appointment"),
+    path('appointments', views.list_appointments,
+         name="list_appointments")
 ]
