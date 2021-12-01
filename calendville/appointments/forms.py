cc2f8f383ad7399b01c58f4fe1bdb1ca654cfdda
Steven Fernández <vsteven25@gmail.com>
2021-12-01 01:10:17 -0600

    Use LoginForm in login view

--- calendville/appointments/forms.py
+++ calendville/appointments/forms.py
@@ -2,8 +2,12 @@ from datetime import datetime, timedelta
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-from appointments.models import Appointment, HealthProcedure
-from appointments.models import Patient
+from appointments.models import (
+    Appointment,
+    HealthProcedure,
+    Patient,
+    Worker,
+)
 
 
 class RegisterAppointmentForm(forms.ModelForm):
@@ -93,18 +97,17 @@ class RegisterPatientForm(forms.ModelForm):
 
 
 class LoginForm(forms.Form):
-    email = forms.CharField(
+    email = forms.EmailField(
         label="Correo", max_length=254, required=True,
-        widget=forms.TextInput(attrs={
+        widget=forms.EmailInput(attrs={
             "class": "form-control",
             "id": "email"
         })
     )
     password = forms.CharField(
         label="Contraseña", required=True,
-        widget=forms.TextInput(attrs={
+        widget=forms.PasswordInput(attrs={
             "class": "form-control",
-            "type": "password",
             "id": "password"
         })
     )
@@ -115,3 +118,16 @@ class LoginForm(forms.Form):
 
     def clean(self):
         super(LoginForm, self).clean()
+        if len(self.cleaned_data) < 2:
+            return
+
+        email = self.cleaned_data["email"]
+        user_entry = Worker.objects.filter(email=email)
+        if not user_entry.exists():
+            error = u"El correo no existe en el sistema"
+            self.add_error("email", error)
+
+        next = self.cleaned_data["next"]
+        if len(next) > 0 and not next.startswith("/"):
+            error = u"No es posible redireccionar a sitios externos"
+            self.add_error("next", error)
